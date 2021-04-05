1a042a3272fdbdfb7a246b5df461ef339334831a
Benjamin Grimes <42780508+EisSchnee@users.noreply.github.com>
2021-04-05 14:53:30 -0500

    Add files via upload

---
+++ foodSense/src/main/java/FoodSense/FoodSenseApplication.java
@@ -0,0 +1,13 @@
+package FoodSense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSenseApplication.class, args);
+	}
+
+}
---
+++ foodSense/src/test/java/FoodSense/FoodSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package FoodSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
