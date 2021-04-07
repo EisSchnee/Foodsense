934e9823e30ce2d128b6427e8230f88fe2802f69
Benjamin Grimes <42780508+EisSchnee@users.noreply.github.com>
2021-04-06 21:54:35 -0500

    Add files via upload

--- foodSense/src/main/java/FoodSense/InventoryService.java
+++ foodSense/src/main/java/FoodSense/InventoryService.java
@@ -1,4 +1,36 @@
 package FoodSense;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * @author Benjamin Grimes
+ */
 public class InventoryService {
+
+
+
+    public LinkedList<Integer> getActiveIDs() {
+        ArrayList<Items.Item> allItems = Items.getItems();
+        LinkedList<Integer> IDs = new LinkedList<>();
+        for(int i = 0; i < allItems.size(); i++){
+            IDs.add(allItems.get(i).getItemID());
+        }
+        return IDs;
+    }
+
+
+    public ArrayList<Double> getPrices(ArrayList<Integer> IDs){
+        ArrayList<Items.Item> allItems = Items.getItems();
+        ArrayList<Double> prices = new ArrayList<>();
+        prices.ensureCapacity(IDs.size());
+        for(int i = 0; i < allItems.size(); i++){
+            for(int j = 0; j < IDs.size(); j++){
+                if(allItems.get(i).getItemID() == IDs.get(j)){
+                    prices.set(j, allItems.get(i).getItemPrice());
+                }
+            }
+        }
+        return prices;
+    }
 }
