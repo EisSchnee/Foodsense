75ed237e9b7146eaea14c87499d854a8cff5be2e
jgriese <34010161+jgriese@users.noreply.github.com>
2021-04-24 21:01:47 -0400

    Updated InventoryModel

--- foodSense/src/main/java/FoodSense/InventoryModel.java
+++ foodSense/src/main/java/FoodSense/InventoryModel.java
@@ -1,25 +1,50 @@
+package inventory;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
+/**
+ * @author Joseph Grieser
+ */
 
 public class InventoryModel {
 
     private HashMap<Integer,Item> items;
 
+    /**
+     * Holds the inventory
+     */
     public InventoryModel(){
         items = new HashMap<>();
     }
 
+    /**
+     * Adds item to inventory
+     * @param item item to be added to inventory
+     */
     public void addItem(Item item){
         items.put(item.getItemID(),item);
     }
 
-    public ArrayList<String> getItems() {
-        return null;
+    /**
+     * Retrieves list of items in inventory
+     * @return list of items in inventory
+     */
+    public ArrayList<Item> getItems() {
+        ArrayList<Item> list = new ArrayList<Item>(items.values());
+        return list;
     }
 
+    /**
+     * Removes item in invventory
+     * @param ID ID of item to be removed
+     * @return removed item
+     */
     public Item removeItem(int ID){
-        return items.remove(ID);
+        //return items.remove(ID);
+        items.get(ID).removeFromStock();
+        Item temp = items.get(ID);
+        return temp;
     }
-
 
 }
