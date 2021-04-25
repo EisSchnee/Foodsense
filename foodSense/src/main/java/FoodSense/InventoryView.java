684dee85c746add97a45a40ffd6332bbafea8e78
jgriese <34010161+jgriese@users.noreply.github.com>
2021-04-24 21:02:50 -0400

    Updated InventoryView

--- foodSense/src/main/java/FoodSense/InventoryView.java
+++ foodSense/src/main/java/FoodSense/InventoryView.java
@@ -1,6 +1,36 @@
+package inventory;
+
+import java.util.ArrayList;
+
+/**
+ * @author Joseph Grieser
+ */
 public class InventoryView {
 
-    public void displayInventory(){
+    private ArrayList<Item> inventory;
 
+    public InventoryView(){
+        inventory = null;
+    }
+
+    public void updateInventory(ArrayList<Item> newList){
+        inventory = newList;
+    }
+
+    public String displayInventory(){
+        //System.out.println("Inventory:\n");
+        //for(com.example.demo.Item i: inventory){
+        //    System.out.println(i.toString());
+        //}
+        String output = "Inventory:\n\n";
+        if(inventory == null){
+            output = output + "No items";
+        }
+        else {
+            for (Item i : inventory) {
+                output = output + i.toString();
+            }
+        }
+        return output;
     }
 }
